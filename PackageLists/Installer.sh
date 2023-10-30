d8f1e52262faee7feb6891dc1020be5d07a9f3e9
ihz28 <149298518+ihz28@users.noreply.github.com>
2023-10-31 04:01:45 +0800

    Update Installer.sh

--- PackageLists/Installer.sh
+++ PackageLists/Installer.sh
@@ -3,6 +3,32 @@
 # Choose hostname
 read -p 'hostname: ' hostname
 
+reflector --country AU --latest 5 --sort rate --save /etc.pacman.d/mirrorlist
+
+mkfs.fat -F 32 -n BOOT /dev/nvme0n1p1
+mkfs.btrfs -L ROOT -f /dev/nvme0n1p2
+mkswap /dev/nvme0n1p3
+mount /dev/nvme0n1p2 /mnt
+cd /mnt
+btrfs su cr @
+btrfs su cr @home
+btrfs su cr @snapshots
+btrfs su cr @log
+btrfs su cr @cache
+cd
+umount /mnt
+mount -o compress=zstd:1,noatime,subvol=@ /dev/nvme0n1p2 /mnt
+mount --mkdir -o compress=zstd:1,noatime,subvol=@home /dev/nvme0n1p2 /mnt/home
+mount --mkdir -o compress=zstd:1,noatime,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
+mount --mkdir -o compress=zstd:1,noatime,subvol=@log /dev/nvme0n1p2 /mnt/var/log
+mount --mkdir -o compress=zstd:1,noatime,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
+mount --mkdir /dev/nvme0n1p1 /mnt/boot/efi
+swapon /dev/nvme0n1p3
+
+pacstrap -K /mnt base linux linux-firmware git reflector
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
 # localization and time setting
 ln -sf /usr/share/zoneinfo/Australia/Perth /etc/localtime
 hwclock --systohc
@@ -19,7 +45,7 @@ pacman -Syy
 
 # INSTALL PACKAGES
 pacman -S --noconfirm --needed - < Base.txt
-pacman -S --noconfirm --needed - < Wayland_files.txt
+pacman -S --noconfirm --needed - < Xorg_files.txt
 pacman -S --noconfirm --needed - < fonts_lists.txt
 
 # Setup GRUB bootloader and Nvidia drivers
