aac4840fd9b24eef0f7b0ce0aeb9436e770e4ace
ihz28 <149298518+ihz28@users.noreply.github.com>
2023-10-31 04:37:08 +0800

    Update Installer.sh

--- PackageLists/Installer.sh
+++ PackageLists/Installer.sh
@@ -5,29 +5,6 @@ read -p 'hostname: ' hostname
 
 reflector --country AU --latest 5 --sort rate --save /etc.pacman.d/mirrorlist
 
-mkfs.fat -F 32 -n BOOT /dev/nvme0n1p1
-mkfs.btrfs -L ROOT -f /dev/nvme0n1p2
-mkswap /dev/nvme0n1p3
-mount /dev/nvme0n1p2 /mnt
-cd /mnt
-btrfs su cr @
-btrfs su cr @home
-btrfs su cr @snapshots
-btrfs su cr @log
-btrfs su cr @cache
-cd
-umount /mnt
-mount -o compress=zstd:1,noatime,subvol=@ /dev/nvme0n1p2 /mnt
-mount --mkdir -o compress=zstd:1,noatime,subvol=@home /dev/nvme0n1p2 /mnt/home
-mount --mkdir -o compress=zstd:1,noatime,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
-mount --mkdir -o compress=zstd:1,noatime,subvol=@log /dev/nvme0n1p2 /mnt/var/log
-mount --mkdir -o compress=zstd:1,noatime,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
-mount --mkdir /dev/nvme0n1p1 /mnt/boot/efi
-swapon /dev/nvme0n1p3
-
-pacstrap -K /mnt base linux linux-firmware git reflector
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
 
 # localization and time setting
 ln -sf /usr/share/zoneinfo/Australia/Perth /etc/localtime
@@ -74,7 +51,7 @@ systemctl enable NetworkManager
 systemctl enable firewalld
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
-systemctl start /dev/zram0
+
 
 echo
 echo "Setup Complete!!"
