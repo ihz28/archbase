264cefaf138b6ee6ebe7cf692ccb2dd14a45e196
ihz28 <149298518+ihz28@users.noreply.github.com>
2023-10-31 20:14:03 +0800

    Update Preinstall.sh

--- PackageLists/Preinstall.sh
+++ PackageLists/Preinstall.sh
@@ -23,4 +23,4 @@ swapon /dev/nvme0n1p3
 pacstrap -K /mnt base linux linux-firmware git reflector
 genfstab -U /mnt >> /mnt/etc/fstab
 sed -i '/subvolid=/s/subvolid=[^ ,]*,//g' /mnt/etc/fstab
-arch-chroot /mnt
+arch-chroot /mnt ./Installer.sh
