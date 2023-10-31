58f41812c304bf6e1e2055f348c1f675762676e7
ihz28 <149298518+ihz28@users.noreply.github.com>
2023-11-01 02:06:45 +0800

    Update Preinstall.sh

--- PackageLists/Preinstall.sh
+++ PackageLists/Preinstall.sh
@@ -13,14 +13,14 @@ btrfs su cr @cache
 cd
 umount /mnt
 mount -o compress=zstd:1,noatime,subvol=@ /dev/nvme0n1p2 /mnt
-mount --mkdir -o compress=zstd:1,noatime,subvol=@home /dev/nvme0n1p2 /mnt/home
-mount --mkdir -o compress=zstd:1,noatime,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
-mount --mkdir -o compress=zstd:1,noatime,subvol=@log /dev/nvme0n1p2 /mnt/var/log
-mount --mkdir -o compress=zstd:1,noatime,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
+mount --mkdir -o compress=zstd,noatime,subvol=@home /dev/nvme0n1p2 /mnt/home
+mount --mkdir -o compress=zstd,noatime,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
+mount --mkdir -o compress=zstd,noatime,subvol=@log /dev/nvme0n1p2 /mnt/var/log
+mount --mkdir -o compress=zstd,noatime,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
 mount --mkdir /dev/nvme0n1p1 /mnt/boot/efi
 swapon /dev/nvme0n1p3
 
 pacstrap -K /mnt base base-devel linux linux-firmware git reflector rsync linux-headers
 genfstab -U /mnt >> /mnt/etc/fstab
 sed -i '/subvolid=/s/subvolid=[^ ,]*,//g' /mnt/etc/fstab
-arch-chroot /mnt
+arch-chroot /mnt ./Installer.sh
