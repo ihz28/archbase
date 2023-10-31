2141e8e7e434410d9217ecebd13af2c4e70ac7d3
ihz28 <149298518+ihz28@users.noreply.github.com>
2023-10-31 18:15:50 +0800

    Update Setup.sh

--- PackageLists/Setup.sh
+++ PackageLists/Setup.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-mkfs.fat -F 32 -n BOOT /dev/nvme0n1p1
-mkfs.btrfs -L ROOT -f /dev/nvme0n1p2
-mkswap /dev/nvme0n1p3
-mount /dev/nvme0n1p2 /mnt
-cd /mnt
-btrfs su cr @
-btrfs su cr @home
-btrfs su cr @snapshots
-btrfs su cr @log
-btrfs su cr @cache
-cd
-umount /mnt
-mount -o compress=zstd:1,noatime,subvol=@ /dev/nvme0n1p2 /mnt
-mount --mkdir -o compress=zstd:1,noatime,subvol=@home /dev/nvme0n1p2 /mnt/home
-mount --mkdir -o compress=zstd:1,noatime,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
-mount --mkdir -o compress=zstd:1,noatime,subvol=@log /dev/nvme0n1p2 /mnt/var/log
-mount --mkdir -o compress=zstd:1,noatime,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
-mount --mkdir /dev/nvme0n1p1 /mnt/boot/efi
-swapon /dev/nvme0n1p3
+git clone https://aur.archlinux.org/yay
+cd yay
+yes | makepkg -si
+yay
 
-pacstrap -K /mnt base linux linux-firmware git reflector
-genfstab -U /mnt >> /mnt/etc/fstab
-sed -i '/subvolid=/s/subvolid=[^ ,]*,//g' /mnt/etc/fstab
-arch-chroot /mnt
+yay -S --noconfirm snapper-support
+
+sudo -s
+cd /
+umount /.snapshots
+rm -r /.snapshots
+snapper -c root create-config /
+btrfs subvolume delete /.snapshots
+
+mkdir /.snapshots
+mount -a
+
+sed -i '22s/""/"wheel"/' /etc/snapper/configs/root
+sed -i '44s/"yes"/"no"/' /etc/snapper/configs/root
+sed -i '51s/"10"/"5"/' /etc/snapper/configs/root
+sed -i '52s/"10"/"5"/' /etc/snapper/configs/root
+sed -i '54s/"10"/"0"' /etc/snapper/configs/root
+sed -i '55s/"10"/"0"/' /etc/snapper/configs/root
+
+chown -R :wheel /.snapshots
+snapper -c root create -d "***Base System***"
+grub-mkconfig -o /boot/grub/grub.cfg
+
+reboot
